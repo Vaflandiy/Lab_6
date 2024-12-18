018136288d0d3ab9cbe038606342a5ab66b4fd5b
Vaflandiy <136570152+Vaflandiy@users.noreply.github.com>
2024-12-18 13:05:05 +0300

    Update _3.cpp

--- _3.cpp
+++ _3.cpp
@@ -12,37 +12,45 @@ int main()
     int min_sum, index;
     min_sum = index = 1000000;
     int sum_for_columns[Number_Maximum] = {0};
-    
     int matrix[Number_Maximum][Number_Maximum];
     int n, m;
-    cout << "Введите размерность матрицы n x m" << endl;
+    cout << "Введите размер матрицы n(строки) x m(столбцы)" << endl; 
     cin >> n >> m;
     
-    for (int i = 0; i < n; i = i + 1) {
-        for (int j = 0; j < m; j = j + 1) {
+    for (int i = 0; i < n; i = i + 1) 
+    {
+        for (int j = 0; j < m; j = j + 1) 
+        {
             cin >> matrix[i][j];
         }
     }
     
-    for (int i = 0; i < n; i = i + 1) {
-        for (int j = 0; j < m; j = j + 1) {
+    for (int i = 0; i < n; i = i + 1) 
+    {
+        for (int j = 0; j < m; j = j + 1) 
+        {
             sum_for_columns[j] = sum_for_columns[j] + matrix[i][j];
         }
     }
     
-    for (int i = 0; i < m; i = i + 1) {
-        if (sum_for_columns[i] < min_sum) {
+    for (int i = 0; i < m; i = i + 1) 
+    {
+        if (sum_for_columns[i] < min_sum) 
+        {
             min_sum = sum_for_columns[i];
             index = i;
         };
     }
     
-    for (int i = 0; i < n; i = i + 1) {
+    for (int i = 0; i < n; i = i + 1) 
+    {
         matrix[i][index] = matrix[i][index] + 3;
     }
 
-    for (int i = 0; i < n; i = i + 1) {
-        for (int j = 0; j < m; j = j + 1) {
+    for (int i = 0; i < n; i = i + 1) 
+    {
+        for (int j = 0; j < m; j = j + 1) 
+        {
             cout << matrix[i][j] << " ";
         }
         cout << endl;
